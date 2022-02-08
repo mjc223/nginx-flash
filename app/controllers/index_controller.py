8a2f09d8ebe356b492ed35823c285e59a38a1a52
Michael <mjc223@njit.edu>
2022-02-08 10:45:13 -0500

    Test commit

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "Michael"
+        name = "Michael Tes"
         return render_template('index.html', name=name)
